e69c9a91fda8d5c62175009c95c9dc5294fff864
BDS <113209967+BDS-001@users.noreply.github.com>
2023-09-02 15:02:35 -0400

    removed redundant code

--- sudoku-solver.js
+++ sudoku-solver.js
@@ -6,14 +6,13 @@ numberBoxes.forEach(function(numberBox) {
     // Add an event listener to the input field for the "keydown" event
     numberBox.addEventListener('keypress', function (event) {
         // Get the key code of the pressed key
+        event.preventDefault();
         const keyCode = event.charCode;
 
         // Check if the key is a number key (0-9)
         if ((keyCode >= 49 && keyCode <= 57 && event.key !== '0')) {
-            event.preventDefault()
             numberBox.value = event.key
         } else {
-            event.preventDefault();
             numberBox.value = ''
         }
     });
